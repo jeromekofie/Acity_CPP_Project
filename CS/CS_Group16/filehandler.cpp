f8a3d072099909576ada9e22ef83f1d9b29b8e20
Katiase19 <111491755+Katiase19@users.noreply.github.com>
2024-12-02 22:40:47 +0000

    Update filehandler.cpp

--- CS/CS_Group16/filehandler.cpp
+++ CS/CS_Group16/filehandler.cpp
@@ -19,22 +19,3 @@ public:
             cout << "Failed to create file.\n";
         }
     }
-
-    void writeFile(string text) {
-        ofstream file(filename); // Overwrites the file
-        if (file) {
-            file << text << endl;
-            cout << "Text written to file.\n";
-        } else {
-            cout << "Failed to write to file.\n";
-        }
-    }
-
-    void deleteFile() {
-        if (remove(filename.c_str()) == 0) {
-            cout << "File deleted.\n";
-        } else {
-            cout << "Failed to delete file.\n";
-        }
-    }
-};
