5901b773e5910363ed9ff8b0c259401c9da8218d
Robert-Owoo <owoorobertgeorge@gmail.com>
2024-12-03 14:28:02 +0000

    final final

--- IT/IT_AI_Group16/Employee Management/Employee.cpp
+++ IT/IT_AI_Group16/Employee Management/Employee.cpp
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
 class Employee {
@@ -10,7 +12,7 @@ public:
     int id;
     string name;
     int age;
-    string dob;  // Date of Birth
+    string dob;  
     string gender;
     string department;
 
@@ -18,15 +20,16 @@ public:
     Employee() : id(0), age(0) {}
 
     // Method to display employee details
-    void display() const {
-        cout << "\nEmployee ID: " << id
-             << "\nName: " << name
-             << "\nAge: " << age
-             << "\nDate of Birth: " << dob
-             << "\nGender: " << gender
-             << "\nDepartment: " << department
-             << "\n----------------------------\n";
-    }
+   void display() const {
+    cout << setw(15) << left << "Employee ID: " << setw(10) << right << id << endl;
+    cout << setw(15) << left << "Name: " << setw(20) << left << name << endl;
+    cout << setw(15) << left << "Age: " << setw(10) << right << age << endl;
+    cout << setw(15) << left << "Date of Birth: " << setw(15) << left << dob << endl;
+    cout << setw(15) << left << "Gender: " << setw(10) << left << gender << endl;
+    cout << setw(15) << left << "Department: " << setw(20) << left << department << endl;
+    cout << "----------------------------\n";
+}
+
 };
 
 #endif // EMPLOYEE_H
