a470929f9219dedb93d55206a4fdb0ec8ea87f36
jedalynn <jess.dodoo@gmail.com>
2024-12-03 10:03:38 -1200

    main2
    reworked on the main

--- CS/CS_Group7/mainclass.cpp
+++ CS/CS_Group7/mainclass.cpp
@@ -1,27 +1,78 @@
 #include <iostream>
+#include "Contact.cpp"
 
 using namespace std;
 
+void displayMenu() {
+    cout << "Contact Management System" << endl;
+    cout << "1. Add Contact" << endl;
+    cout << "2. Show All Contacts" << endl;
+    cout << "3. Search Contact" << endl;
+    cout << "4. Delete Contact" << endl;
+    cout << "5. Sort Contacts by Name" << endl;
+    cout << "6. Sort Contacts by Phone Number" << endl;
+    cout << "7. Exit" << endl;
+    cout << "Enter your choice: ";
+}
 
 int main() {
-   
-    //ContactManager manager;
     int choice;
 
     do {
-        cout << "\nContact Management System\n";
-        cout << "1. Add Contact\n";
-        cout << "2. Search Contact\n";
-        cout << "3. Delete Contact\n";
-        cout << "4. Sort Contacts by Name\n";
-        cout << "6. Display All Contacts\n";
-        cout << "7. Exit\n";
-        cout << "Enter your choice: ";
+        displayMenu();
         cin >> choice;
 
-  
-    }while(choice=!7);
-    cout<<"Wrong input\n";
-    
+        switch (choice) {
+            case 1: {
+                Contact newContact;
+                newContact.createContact();
+                if (Contact::contactExists(newContact.getPhoneNumber())) {
+                    cout << "A contact with this phone number already exists!" << endl;
+                } else {
+                    newContact.writeToFile();
+                    cout << "Contact added successfully!" << endl;
+                }
+                break;
+            }
+            case 2:
+                Contact::displayAllContacts();
+                break;
+            case 3: {
+                long long phone;
+                cout << "Enter phone number to search: ";
+                cin >> phone;
+                if (Contact::contactExists(phone)) {
+                    cout << "Contact found and displayed above!" << endl;
+                } else {
+                    cout << "Contact not found!" << endl;
+                }
+                break;
+            }
+            case 4: {
+                long long phone;
+                cout << "Enter phone number to delete: ";
+                cin >> phone;
+                if (Contact::deleteContact(phone)) {
+                    cout << "Contact deleted successfully!" << endl;
+                } else {
+                    cout << "Contact not found!" << endl;
+                }
+                break;
+            }
+            case 5:
+                Contact::sortContacts(true);
+                break;
+            case 6:
+                Contact::sortContacts(false);
+                break;
+            case 7:
+                cout << "Exiting Contact Management System. Goodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid choice! Please try again." << endl;
+        }
+    } while (choice != 7);
+
     return 0;
 }
+
