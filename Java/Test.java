15e6c2d167f3018f168fbee8a865a18160a42d00
Zanetta-15 <136020921+Zanetta-15@users.noreply.github.com>
2025-02-14 10:24:06 +0000

    group 4
    group 4 task

--- Java/Test.java
+++ Java/Test.java
@@ -17,6 +17,30 @@ public class Test{
     //Group 3
 
     //Group 4
+        public static void runMemoryTest() throws InterruptedException {
+        Scanner keyboard = new Scanner(System.in);
+        Random randomNumbers = new Random();
+
+        int number = randomNumbers.nextInt(10) + 1;
+        System.out.println(number);
+        
+        Thread.sleep(2000);
+
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.print("Recall the number: ");
+        int recall = keyboard.nextInt();
+
+        if (recall == number) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Incorrect! The number was: " + number);
+        }
+
+        keyboard.close();
+    }
+
 
     //Group 5
 
