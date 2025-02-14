d959d598da5d79ac0f730acac94252a435e0b63c
Josavina1 <josavina.abbey@acity.edu.gh>
2025-02-14 10:05:40 +0000

    Update Test.java
    The first code

--- Java/Test.java
+++ Java/Test.java
@@ -12,6 +12,26 @@ public class Test{
 
     //Group 1
 
+public class the_quiz {
+    public static String combinePasswords(String group3, String group5) {
+        return group3 + group5;
+    }
+
+    public static String getPasswordInput() {
+        return JOptionPane.showInputDialog(null,"Please enter the name password:");
+    }
+
+    public static void checkPassword(String inputPassword, String fullPassword) {
+        if (inputPassword.equals(fullPassword)) {
+
+            JOptionPane.showMessageDialog(null, "Access granted! The door opens.");
+        } else {
+
+            JOptionPane.showMessageDialog(null, "Access denied. Incorrect password.");
+        }
+    }
+}
+
 
     //Group 2
 
