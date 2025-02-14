8df3c994f63f24930ee293ccb259ecc7483a78ec
Ranajart <najart.rauf@acity.edu.gh>
2025-02-14 10:35:27 +0000

    Update Test.java
    group 1

--- Java/Test.java
+++ Java/Test.java
@@ -11,7 +11,25 @@ public class Test{
     }
 
     //Group 1
+    public class NameCodeLock {
+        public static String combinePasswords(String group3, String group5) {
+            return group3 + group5;
+        }
     
+        public static String getPasswordInput() {
+            return JOptionPane.showInputDialog(null,"Please enter the name password:");
+        }
+    
+        public static void checkPassword(String inputPassword, String fullPassword) {
+            if (inputPassword.equals(fullPassword)) {
+    
+                JOptionPane.showMessageDialog(null, "Access granted! The door opens.");
+            } else {
+    
+                JOptionPane.showMessageDialog(null, "Access denied. Incorrect password.");
+            }
+        }
+    }
     //Group 2
     public static void group2() {
     
