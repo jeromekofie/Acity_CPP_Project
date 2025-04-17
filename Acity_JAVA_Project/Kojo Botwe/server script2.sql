4c20a9a9b058f7fa2ff35ed827e14b7612f14bfd
Kbotwe7 <kojobaafibotwe@gmail.com>
2025-04-17 11:31:48 +0000

    Add files via upload

---
+++ Acity_JAVA_Project/Kojo Botwe/server script2.sql
@@ -0,0 +1,17 @@
+-- select * from tasks;
+-- CREATE TABLE users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     username VARCHAR(100) NOT NULL UNIQUE,
+--     password VARCHAR(100) NOT NULL
+-- );
+
+-- INSERT INTO users (username, password) VALUES ('admin', 'admin123');-- 
+
+
+-- INSERT INTO users (username, password) VALUES ('KBB', 'cyrilpp');
+
+
+
+
+
+select * from tasks;
