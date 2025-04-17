1a8f55a2ddb7f2f9e0b8992a78f83e05715a234d
kappiah85 <kappiah8415@gmail.com>
2025-04-17 14:05:14 +0000

    Add files via upload

---
+++ Acity_JAVA_Project/Kofi Appiah.10022300106/KofiAppiah/HealthManager/src/healthmanager/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package healthmanager;
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String[] args) {
+    javax.swing.SwingUtilities.invokeLater(() -> new Login());
+    }
+}
