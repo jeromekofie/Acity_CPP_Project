365c6a119eeba26eca5f473f86242a2db8ca682a
joshuaveve <jamenuveve6@gmail.com>
2025-04-17 13:28:40 +0000

    Finder

---
+++ Acity_JAVA_Project/Ebere Prince Enoch/SearchLLC/src/FileHandler.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileHandler {
+    public ArrayList<String> readFile(String filename) {
+        ArrayList<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
